a7997960c4e7228da4afb20f3e78e33e3716aec7
Jhany <jhany_rev@hotmail.com>
2021-06-14 12:55:08 -0500

    eliminación front de prueba

--- front/src/config/index.js
+++
@@ -1,4 +0,0 @@
-export const uri = {
-    local: "http://192.168.1.40:8080/no_circula",
-  };
-  
--- front/src/http-common.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { uri }  from './config/index';
-
-export default axios.create({
-  baseURL: uri.local,
-  headers: {
-    "Content-type": "application/json"
-  }
-});
